a9185cd9e79ab18f5cdc4dd6fc9649388e2af54f
na <25114018+Natronium@users.noreply.github.com>
2023-04-10 23:42:56 -0400

    Implement SafetyRace plugin
    This plugin lets users reshuffle their decks when reloading specific
    events. It accomplishes fairly straightforwardly, by watching for the
    game loading and then manually reshuffling the player and npc decks.
    
    Additionally it modifies the menu buttons to make it obvious that a mod
    is loaded: "Begin Story" becomes "Begin Race" "Continue Story" becomes
    "Continue Story*" and, if one of the matched events is being reloaded,
    "Continue Story" becomes "Reshuffle"
    
    There's some fiddliness with the transparency on the menu button text,
    caused, I think, by the fade-in animation on the buttons when the game
    first starts. The end result is that all the TextMeshPro text objects
    have an alpha of 0 (transparent), but this doesn't get applied until we
    change the literal text. So we also need to make sure to set the alpha
    to 1 (except not during that initial fade in animation since that looks
    wonky)

--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,95 @@
-﻿using BepInEx;
+﻿using System.Collections.Generic;
+using BepInEx;
+using Echodog;
+using HarmonyLib;
+using TMPro;
 
 namespace SafetyRace
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+  [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+  public class Plugin : BaseUnityPlugin
+  {
+    static Plugin Instance;
+    static bool JustLoaded;
+    static bool IsFirstLoad = true;
+
+    static List<string> ReshuffleEvents = new List<string>() {
+        "gull_intro", "talia_fish", "oscar_welcome",
+        "lars_intro", "maya_welcome"
+    };
+
+    private void Awake()
     {
-        private void Awake()
-        {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-        }
+      Instance = this;
+      var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+      harmony.PatchAll(); // automatic based on HarmonyPatch &c attributes
+      Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
     }
+
+    [HarmonyPatch(typeof(Echodog.TitleController))]
+    private static class TitleControllerPatch
+    {
+
+      [HarmonyPostfix]
+      [HarmonyPatch("LoadGame")]
+      static void PostLoadGame()
+      {
+        Instance.Logger.LogDebug("Game reload detected: watching for next event");
+        JustLoaded = true;
+      }
+
+      [HarmonyPostfix]
+      [HarmonyPatch("Go")]
+      static void PostGo()
+      {
+        TextMeshPro firstButtonText = TitleController.Instance.texts[0];
+        if (firstButtonText.text == "BEGIN STORY")
+        {
+          firstButtonText.text = "BEGIN RACE";
+        }
+        else if (firstButtonText.text == "CONTINUE STORY" && ReshuffleEvents.Contains(SaveManager.Instance.currentEvent))
+        {
+          firstButtonText.text = "RESHUFFLE";
+        }
+        else
+        {
+          firstButtonText.text = "CONTINUE STORY*";
+        }
+
+        if (!IsFirstLoad)
+        {
+          // all the TMP button text has alpha 0 by default, and updating the text redraws it transparent as a result
+          firstButtonText.alpha = 1;
+        }
+        else
+        {
+          IsFirstLoad = false;
+        }
+
+      }
+    } //hook TitleController.LoadGame & Go
+
+    [HarmonyPatch(typeof(Echodog.GameController))]
+    private static class GameControllerPatch
+    {
+
+      [HarmonyPostfix]
+      [HarmonyPatch("Init")]
+      static void PostInit(GameController __instance, EventData eventData)
+      {
+        if (JustLoaded)
+        {
+          JustLoaded = false;
+          if (ReshuffleEvents.Contains(eventData.id))
+          {
+            Instance.Logger.LogDebug($"Reshuffling decks for event {eventData.id}");
+            __instance.runner.game.player.deck.ShuffleInPlace();
+            __instance.runner.game.npc.deck.ShuffleInPlace();
+          }
+        }
+      }
+
+
+    } //hook GameController.Init
+  }
 }
